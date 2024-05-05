0e8067c2fce6b978d0f2842c5fcafb2ce2c95fa0
daniele-rosati <drosati151@gmail.com>
2024-05-05 17:41:36 +0200

    random number

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$password = "";
+for ($i=0; $i < 10 ; $i++) { 
+
+    $rndomNum = (string) rand(0, 9);
+    $password = $password . $rndomNum;
+
+};
+
+echo $password; 
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
